a6d0e3ae0fc516c59928f0c9cc81be623b45b6a2
Austine <austineskylah9195@gmail.com>
2021-04-13 17:46:19 +0300

    Added CRUD to Article Module

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -1,7 +1,20 @@
 # articles/urls.py
 from django.urls import path
-from .views import ArticleListView
+from .views import (
+     ArticleListView,
+     ArticleUpdateView,
+    ArticleDetailView,
+    ArticleDeleteView,
+    ArticleCreateView
+)
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
+    path('<int:pk>/edit/',
+        ArticleUpdateView.as_view(), name='article_edit'), # new
+path('<int:pk>/',
+        ArticleDetailView.as_view(), name='article_detail'), # new
+path('<int:pk>/delete/',
+        ArticleDeleteView.as_view(), name='article_delete'), # new
+path('new/', ArticleCreateView.as_view(), name='article_new')
 ]
