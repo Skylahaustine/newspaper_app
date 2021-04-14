82548c73bb2fabae3151c414cdb170d3df07c6b3
Austine <austineskylah9195@gmail.com>
2021-04-14 17:51:01 +0300

    Added Authorization and Comment Functionality

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,27 +1,42 @@
 # articles/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView # new
 from django.views.generic.edit import UpdateView, DeleteView,CreateView # new
 from django.urls import reverse_lazy # new
 
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(DetailView): # new
+class ArticleDetailView(LoginRequiredMixin,DetailView): # new
     model = Article
     template_name = 'article_detail.html'
-class ArticleUpdateView(UpdateView): # new
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView): # new
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
-class ArticleDeleteView(DeleteView): # new
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView): # new
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-class ArticleCreateView(CreateView):
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model=Article
     template_name='article_new.html'
-    fields=('title', 'body', 'author',)
+    fields=('title', 'body')
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
